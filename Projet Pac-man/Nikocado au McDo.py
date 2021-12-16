63aaedb301ddab128bc84b515234de54813dd252
Akshay <90514084+KessieSeMoqueDUnCaissier@users.noreply.github.com>
2021-12-16 23:49:54 +0100

    Update Nikocado au McDo.py

--- Projet Pac-man/Nikocado au McDo.py
+++ Projet Pac-man/Nikocado au McDo.py
@@ -1,6 +1,6 @@
 import pygame # importation de la librairie pygame
 import Classes
-import math
+import random
 import sys # pour fermer correctement l'application
 
 # lancement des modules inclus dans pygame
@@ -11,7 +11,7 @@ FPS=400
 
 # création d'une fenêtre de 800 par 600
 screen = pygame.display.set_mode((800,600))
-pygame.display.set_caption("Nikocado au McDo") 
+pygame.display.set_caption("The journey of Nikocado") 
 
 # chargement de l'image de fond et du laby
 fond=pygame.image.load("Projet Pac-man\Images\-Amogus.png")
@@ -19,7 +19,10 @@ maze= Classes.Maze()
 
 # creation du joueur et des ennemis
 player = Classes.Joueur()
-
+listeEnnemis = []
+for indice in range(Classes.Ennemi.NbEnnemis):
+    mechant = Classes.Ennemi()
+    listeEnnemis.append(mechant)
 
 ### BOUCLE DE JEU  ###
 running = True # variable pour laisser la fenêtre ouverte
@@ -30,10 +33,8 @@ while running : # boucle infinie pour laisser la fenêtre ouverte
     # joueur en action
     player.health_bar(screen)
     player.update(screen)
-    maze.image_surf = player.image
-    maze._block_surf = pygame.image.load("Projet Pac-man\Images\-MazeBlock.png").convert()
-    maze.burgers= pygame.image.load("Projet Pac-man\Images\-Burger.png")
-    maze.coca= pygame.image.load("Projet Pac-man\Images\-Coca.png")
+    maze.block_surf = pygame.image.load("Projet Pac-man\Images\-MazeBlock.png").convert_alpha()
+
 
     ### Gestion des événements  ###
     for event in pygame.event.get(): # parcours de tous les event pygame dans cette fenêtre
@@ -45,28 +46,51 @@ while running : # boucle infinie pour laisser la fenêtre ouverte
         if event.type == pygame.KEYDOWN : # si une touche a été tapée KEYUP quand on relache la touche
             if event.key == pygame.K_LEFT : # si la touche est la fleche gauche
                 player.sens = "gauche" # on déplace le vaisseau de 1 pixel sur la gauche
-                player.image=pygame.image.load("Projet Pac-man\Images\-NickGauche.png")
+                player.image=pygame.image.load("Projet Pac-man\Images\-NickGauche.png").convert_alpha()
             if event.key == pygame.K_RIGHT : # si la touche est la fleche droite
                 player.sens = "droite" # on déplace le vaisseau de 1 pixel sur la droite
-                player.image=pygame.image.load("Projet Pac-man\Images\-NickDroite.png")
+                player.image=pygame.image.load("Projet Pac-man\Images\-NickDroite.png").convert_alpha()
             if event.key == pygame.K_DOWN: # on déplace le vaisseau de 1 pixel vers le bas
                 player.sens = "bas"
-                player.image=pygame.image.load("Projet Pac-man\Images\-NickBas.png")
+                player.image=pygame.image.load("Projet Pac-man\Images\-NickBas.png").convert_alpha()
             if event.key == pygame.K_UP: # on déplace le vaisseau de 1 pixel vers le haut
                 player.sens = "haut"
-                player.image=pygame.image.load("Projet Pac-man\Images\-NickHaut.png")
+                player.image=pygame.image.load("Projet Pac-man\Images\-NickHaut.png").convert_alpha()
         # musique
             if event.key == pygame.K_m:
                 pygame.mixer.music.load("Projet Pac-man\Sons\Giorno_JJBA.mp3")
                 pygame.mixer.music.play()
+
     ### Actualisation de la scene ###
-    # collisions
-    "collision=maze.burgers.get_rect()"
-    "if collision.colliderect(player.rect)==False:"
+    # joueur
     player.deplacer()
-    screen.blit(maze.image_surf,[player.rect.x,player.rect.y]) # appel de la fonction qui dessine le joueur
-    maze.draw(screen, maze._block_surf) # on dessine le labyrinthe 
-    maze.draw2(screen, maze.burgers)
-    maze.draw3(screen, maze.coca)
+    screen.blit(player.image,player.rect) # appel de la fonction qui dessine le joueur
+    # duo de vilain
+    for ennemi in listeEnnemis:
+        ennemi.deplacer()
+        screen.blit(ennemi.image,ennemi.rect) # appel de la fonction qui dessine les ennemis
+    # labyrinthe
+    maze.draw(screen, maze.block_surf) # on dessine le labyrinthe
+
+    ### Collisions ###
+    # joueur VS murs
+    rectBlock = maze.block_surf.get_rect()
+    if player.rect.colliderect(rectBlock):
+        player.vitesse=0
+    # vilains VS murs & joueur
+    for vilain in listeEnnemis:    
+        if vilain.rect.colliderect(rectBlock):
+            vilain.sens=random.randint(1,4)
+        elif vilain.rect.colliderect(player.rect):
+            player.health-=50
+            player.rect.x=45
+            player.rect.y=514
+            player.sens="O"
+
+    ### Fin de jeu ###
+    if player.health==0:
+           running = False # running est sur False
+           sys.exit() 
+
     pygame.display.update() # pour ajouter tout changement à l'écran
     clock.tick(FPS)
